989c2b8cf51b7cb3adc5b0b09f592a93e0d2a138
Pritam <pritamd47@gmail.com>
2020-04-25 18:35:53 +0530

    moved Tests back to parent directory

---
+++ Tests/test1.py
@@ -0,0 +1,25 @@
+import unittest
+
+from qgis.PyQt import (
+        QtWidgets,
+        uic
+    )
+    
+from qgis.core import (
+    QgsProject,
+    QgsRasterLayer,
+    QgsVectorLayer
+)
+
+from SMProcessing.SMProcessing import SMProcessing
+from .utilities import get_qgis_app
+
+class TestInit(unittest.TestCase):
+    def test_init(self):
+        self.assertIsNotNone(IFACE, None)
+        plugin = SMProcessing(IFACE)
+        print(type(plugin.dlg))
+
+
+if __name__ == "__main__":
+    unittest.main()
